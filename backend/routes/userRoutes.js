57dc901640ac7a9a8049d6181b4ea8d22e0a5871
Heli Ribeiro <heliribeiro2@gmail.com>
2020-10-22 17:20:36 -0300

    adicionada a rota de autenticação

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import User from '../models/User.js'
+import bcrypt from 'bcryptjs'
 
 const router = express.Router()
 
 router.post('/register',async(req,res)=> {
         const {name,username,password} = req.body
     try {
+        if(await User.findOne({username}))
+            return res.status(400).json('O usuário ja existe!')
+
         const user = await User.create({name,username,password})
         user.password = undefined
         res.send(user)
@@ -13,5 +17,24 @@ router.post('/register',async(req,res)=> {
         res.status(400).send({error:'Não foi possível cadastrar o Usuário '+error})
     }
 })
+
+router.post('/authenticate',async(req,res)=>{
+    const {username,password} = req.body
+
+    try {
+        const user = await User.findOne({username}).select('+password')
+        if(!user)
+            res.status(400).send({error:"O usuário não está cadastrado"})
+        
+        if(!await bcrypt.compare(password, user.password))
+           res.status(400).send({error:"Senha inválida"})
+       
+        res.send({user})
+
+    } catch (error) {
+        res.status(400).send({error:'Não foi possível fazer o login '+error})
+    }
+})
+
 
 export default router
