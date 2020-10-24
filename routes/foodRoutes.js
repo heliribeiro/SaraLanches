192f2400704062f2003a6ca24524b904bebcee86
Heli Ribeiro <heliribeiro2@gmail.com>
2020-10-23 22:14:28 -0300

    criação das rotas de updadte,delete e buscar

--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -12,14 +12,54 @@ router.get('/',async(req,res)=>{
     }
 })
 
+router.get('/:id', async(req,res)=>{
+    const {id} = req.params
+    try {
+        const food = await Food.findById(id)
+        res.send({food})
+    } catch (error) {
+        res.status(400).send({error:"Não foi possível buscar o lanche "+error})
+    }
+})
+
 router.post('/',async(req,res)=>{
     const {name,link_picture,price,description} = req.body
     try {
-        const food = await Food.create({name,link_picture,price,description})
+        const food = await Food.find({name})
+        if(food)
+            res.status(400).send("Você ja cadastrou um lanche com esse nome!!")
+            
+        const newFood = await Food.create({name,link_picture,price,description})
 
-        res.send({food})
+        res.send({newFood})
     } catch (error) {
         res.status(400).send({error:"Não foi possível criar o lanche "+error})
+    }
+
+})
+
+router.put('/:id', async(req,res)=>{
+    const {name, link_picture,price,description} = req.body
+    const {id} = req.params
+    try {
+
+        const food = await Food.findByIdAndUpdate(id,{name,link_picture,price,description}, {new:true})
+
+        res.send({food})
+
+    } catch (error) {
+        res.status(400).send({error:"Não foi possível atualizar o lanche"})
+    }
+})
+
+
+router.delete('/:id', async(req,res)=>{
+    const {id} = req.params
+    try {
+        const user = await Food.findByIdAndDelete(id)
+        res.send({ok:"Lanche removido com sucesso!"})
+    } catch (error) {
+        res.status(400).send({error:"Nçao foi possível remover o lanche! "+error})
     }
 })
 
